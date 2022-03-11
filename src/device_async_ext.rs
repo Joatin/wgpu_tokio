71737b295c8b82a7841a8246cb1e8e933f4ed002
Joatin Granlund <granlundjoatin@gmail.com>
2022-03-11 13:17:23 +0100

    added more documentation

--- src/device_async_ext.rs
+++ src/device_async_ext.rs
@@ -1,65 +1,171 @@
 use std::sync::Arc;
+use wgpu::util::{BufferInitDescriptor, DeviceExt};
 use wgpu::{
     BindGroup, BindGroupDescriptor, BindGroupLayout, BindGroupLayoutDescriptor, Buffer,
     BufferDescriptor, CommandEncoder, CommandEncoderDescriptor, ComputePipeline,
     ComputePipelineDescriptor, Device, Error, ErrorFilter, Features, Limits, Maintain,
-    PipelineLayout, PipelineLayoutDescriptor, QuerySet, QuerySetDescriptor, RenderBundleEncoder,
-    RenderBundleEncoderDescriptor, RenderPipeline, RenderPipelineDescriptor, Sampler,
-    SamplerDescriptor, ShaderModule, ShaderModuleDescriptor, ShaderModuleDescriptorSpirV, Texture,
-    TextureDescriptor, UncapturedErrorHandler,
+    PipelineLayout, PipelineLayoutDescriptor, QuerySet, QuerySetDescriptor, Queue,
+    RenderBundleEncoder, RenderBundleEncoderDescriptor, RenderPipeline, RenderPipelineDescriptor,
+    Sampler, SamplerDescriptor, ShaderModule, ShaderModuleDescriptor, ShaderModuleDescriptorSpirV,
+    Texture, TextureDescriptor, UncapturedErrorHandler,
 };
 
+/// This trait adds async versions of the standard methods
+///
+/// The naming convention is the same as the original name, with an "_async" postfix
 #[async_trait::async_trait]
 pub trait DeviceAsyncExt {
+    /// Async version of the `poll` method
+    ///
+    /// See [wgpu::Device::poll]
     async fn poll_async(&self, maintain: Maintain);
+
+    /// Async version of the `features` method
+    ///
+    /// See [wgpu::Device::features]
     async fn features_async(&self) -> Features;
+
+    /// Async version of the `limits` method
+    ///
+    /// See [wgpu::Device::limits]
     async fn limits_async(&self) -> Limits;
+
+    /// Async version of the `create_shader_module` method
+    ///
+    /// See [wgpu::Device::create_shader_module]
     async fn create_shader_module_async(
         &self,
         desc: ShaderModuleDescriptor<'static>,
     ) -> ShaderModule;
+
+    /// Async version of the `create_shader_module_unchecked` method
+    ///
+    /// See [wgpu::Device::create_shader_module_unchecked]
     async unsafe fn create_shader_module_unchecked_async(
         &self,
         desc: ShaderModuleDescriptor<'static>,
     ) -> ShaderModule;
+
+    /// Async version of the `create_shader_module_spirv` method
+    ///
+    /// See [wgpu::Device::create_shader_module_spirv]
     async unsafe fn create_shader_module_spirv_async(
         &self,
         desc: ShaderModuleDescriptorSpirV<'static>,
     ) -> ShaderModule;
+
+    /// Async version of the `create_command_encoder` method
+    ///
+    /// See [wgpu::Device::create_command_encoder]
     async fn create_command_encoder_async(
         &self,
         desc: CommandEncoderDescriptor<'static>,
     ) -> CommandEncoder;
+
+    /// Async version of the `create_render_bundle_encoder` method
+    ///
+    /// See [wgpu::Device::create_render_bundle_encoder]
     async fn create_render_bundle_encoder_async<'a>(
         &'a self,
         desc: RenderBundleEncoderDescriptor<'a>,
     ) -> RenderBundleEncoder<'a>;
+
+    /// Async version of the `create_bind_group` method
+    ///
+    /// See [wgpu::Device::create_bind_group]
     async fn create_bind_group_async(&self, desc: BindGroupDescriptor<'static>) -> BindGroup;
+
+    /// Async version of the `create_bind_group_layout` method
+    ///
+    /// See [wgpu::Device::create_bind_group_layout]
     async fn create_bind_group_layout_async(
         &self,
         desc: BindGroupLayoutDescriptor<'static>,
     ) -> BindGroupLayout;
+
+    /// Async version of the `create_pipeline_layout` method
+    ///
+    /// See [wgpu::Device::create_pipeline_layout]
     async fn create_pipeline_layout_async(
         &self,
         desc: PipelineLayoutDescriptor<'static>,
     ) -> PipelineLayout;
+
+    /// Async version of the `create_render_pipeline` method
+    ///
+    /// See [wgpu::Device::create_render_pipeline]
     async fn create_render_pipeline_async(
         &self,
         desc: RenderPipelineDescriptor<'static>,
     ) -> RenderPipeline;
+
+    /// Async version of the `create_compute_pipeline` method
+    ///
+    /// See [wgpu::Device::create_compute_pipeline]
     async fn create_compute_pipeline_async(
         &self,
         desc: ComputePipelineDescriptor<'static>,
     ) -> ComputePipeline;
+
+    /// Async version of the `create_buffer` method
+    ///
+    /// See [wgpu::Device::create_buffer]
     async fn create_buffer_async(&self, desc: BufferDescriptor<'static>) -> Buffer;
+
+    /// Async version of the `create_texture` method
+    ///
+    /// See [wgpu::Device::create_texture]
     async fn create_texture_async(&self, desc: TextureDescriptor<'static>) -> Texture;
+
+    /// Async version of the `create_sampler` method
+    ///
+    /// See [wgpu::Device::create_sampler]
     async fn create_sampler_async(&self, desc: SamplerDescriptor<'static>) -> Sampler;
+
+    /// Async version of the `create_query_set` method
+    ///
+    /// See [wgpu::Device::create_query_set]
     async fn create_query_set_async(&self, desc: QuerySetDescriptor<'static>) -> QuerySet;
+
+    /// Async version of the `on_uncaptured_error` method
+    ///
+    /// See [wgpu::Device::on_uncaptured_error]
     async fn on_uncaptured_error_async(&self, handler: impl UncapturedErrorHandler);
+
+    /// Async version of the `push_error_scope` method
+    ///
+    /// See [wgpu::Device::push_error_scope]
     async fn push_error_scope_async(&self, filter: ErrorFilter);
+
+    /// Async version of the `pop_error_scope` method
+    ///
+    /// See [wgpu::Device::pop_error_scope]
     async fn pop_error_scope_async(&self) -> Option<Error>;
+
+    /// Async version of the `start_capture` method
+    ///
+    /// See [wgpu::Device::start_capture]
     async fn start_capture_async(&self);
+
+    /// Async version of the `stop_capture` method
+    ///
+    /// See [wgpu::Device::stop_capture]
     async fn stop_capture_async(&self);
+
+    /// Async version of the `create_buffer_init` method
+    ///
+    /// See [wgpu::util::DeviceExt::create_buffer_init]
+    async fn create_buffer_init_async(&self, desc: BufferInitDescriptor<'static>) -> Buffer;
+
+    /// Async version of the `create_texture_with_data` method
+    ///
+    /// See [wgpu::util::DeviceExt::create_texture_with_data]
+    async fn create_texture_with_data_async(
+        &self,
+        queue: &Arc<Queue>,
+        desc: TextureDescriptor<'static>,
+        data: Vec<u8>,
+    ) -> Texture;
 }
 
 #[async_trait::async_trait]
@@ -234,5 +340,29 @@ impl DeviceAsyncExt for Arc<Device> {
 
     async fn stop_capture_async(&self) {
         self.stop_capture()
+    }
+
+    async fn create_buffer_init_async(&self, desc: BufferInitDescriptor<'static>) -> Buffer {
+        let device = Arc::clone(self);
+        let handle = tokio::task::spawn_blocking(move || device.create_buffer_init(&desc));
+
+        // If the inner code panics, we just pass the panic along
+        handle.await.unwrap()
+    }
+
+    async fn create_texture_with_data_async(
+        &self,
+        queue: &Arc<Queue>,
+        desc: TextureDescriptor<'static>,
+        data: Vec<u8>,
+    ) -> Texture {
+        let device = Arc::clone(self);
+        let queue = Arc::clone(queue);
+        let handle = tokio::task::spawn_blocking(move || {
+            device.create_texture_with_data(&queue, &desc, &data)
+        });
+
+        // If the inner code panics, we just pass the panic along
+        handle.await.unwrap()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 mod device_async_ext;
 
 pub use self::device_async_ext::DeviceAsyncExt;
